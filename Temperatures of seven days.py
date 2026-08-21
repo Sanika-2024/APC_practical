7768f2d94860424ca23bfda997df6c95d2a4c8f9
Sanika-2024 <sanika2418nandre@gmail.com>
2026-08-21 02:19:01 -0700

    Delete Temperatures of seven days.py

--- Temperatures of seven days.py
+++
@@ -1,5 +0,0 @@
-temperatures = (30, 32, 29, 35, 31, 33, 28)
-
-print("Maximum temperature:", max(temperatures))
-print("Minimum temperature:", min(temperatures))
-print("Average temperature:", sum(temperatures) / len(temperatures))
