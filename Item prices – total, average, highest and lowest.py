3ab479ffb80cacc5a13bc01e1535d881ba0df10e
Sanika-2024 <sanika2418nandre@gmail.com>
2026-08-21 02:21:38 -0700

    Delete Item prices – total, average, highest and lowest.py

--- Item prices – total, average, highest and lowest.py
+++
@@ -1,9 +0,0 @@
-prices = (100, 250, 300, 150, 500)
-
-total = sum(prices)
-average = total / len(prices)
-
-print("Total bill:", total)
-print("Average price:", average)
-print("Highest price:", max(prices))
-print("Lowest price:", min(prices))
